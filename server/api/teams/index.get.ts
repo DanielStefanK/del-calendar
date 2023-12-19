ea90e13a841aa45f16988287e136810d6363ee59
Daniel Klose <daniel.klose@adesso.de>
2023-12-19 11:55:47 +0100

    feat: added basic pipeline

--- server/api/teams/index.get.ts
+++ server/api/teams/index.get.ts
@@ -1,6 +1,7 @@
 import { scrapeTeamsAndIds } from "~/server/scraper";
 
 export default defineEventHandler((_) => {
+  // eslint-disable-next-line no-console
   console.log(`fetching teams`);
   return scrapeTeamsAndIds();
 });
